a5fe0b0594b2c28739d0f6f9095a276cbf2b4b1c
Jürgen Kudla <kudlaj@gmail.com>
2024-08-29 11:23:31 +0200

    fixing /playground route

--- graphql-playground/src/index.js
+++ graphql-playground/src/index.js
@@ -2,12 +2,27 @@ import { createGraphiQLFetcher } from '@graphiql/toolkit';
 import { GraphiQL } from 'graphiql';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { explorerPlugin } from '@graphiql/plugin-explorer';
 import 'graphiql/graphiql.css';
 import '@graphiql/plugin-explorer/dist/style.css';
-import './index.css'; 
+import './index.css';
 
 const fetcher = createGraphiQLFetcher({ url: process.env.REACT_APP_GQL_URL });
 const explorer = explorerPlugin();
+
+const PlaygroundComponent = () => (
+  <GraphiQL fetcher={fetcher} plugins={[explorer]} />
+);
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/playground" element={<PlaygroundComponent />} />
+      <Route path="*" element={<Navigate to="/playground" replace />} />
+    </Routes>
+  </Router>
+);
+
 const root = createRoot(document.getElementById('root'));
-root.render(<GraphiQL fetcher={fetcher} plugins={[explorer]} />);
+root.render(<App />);
